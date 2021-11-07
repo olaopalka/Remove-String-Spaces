56017659f937ae9fb68bce90a5ab09e0ff4a9259
olaopalka <olaopalka84@gmail.com>
2021-11-07 18:01:50 +0100

    add working function

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-ok = 'hello'
-temp = 0
-for c in list(ok):
-    if(c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'):
-        temp += 1
-print(temp)
-
-
-
-
-# a, e, i, o, u
+def no_space(x):
+    return x.replace(" ", "")
